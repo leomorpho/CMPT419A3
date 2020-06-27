8359dcd3ab5d83fe9fa0fd88fe884520ff47bcc5
leonard audibert <leonard.audibert@gmail.com>
2020-06-27 14:20:31 -0700

    Explore how to solve DTW for GMM

---
+++ run_openface.py
@@ -0,0 +1,14 @@
+import glob
+import subprocess
+import os
+
+directories = glob.glob("images/*")
+output = "output"
+
+for dir in directories:
+    output_dir = "%s/%s" % (output, dir)
+    os.makedirs(output_dir)
+    process = subprocess.Popen(["build/bin/FeatureExtraction", "-fdir", dir, "-out_dir", output_dir])
+    process.wait()
+    
+# build/bin/FeatureExtraction -fdir images/1 -out_dir output
